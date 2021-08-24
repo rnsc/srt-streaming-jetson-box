e404660f40bda9055acbaaba3122396f6040a257
rnsc <github@rnsc.be>
2021-08-24 20:54:52 +0200

    echo fix + handler fix

--- belabox_install.sh
+++ belabox_install.sh
@@ -8,6 +8,6 @@ sudo apt-get install python3-pip -y
 
 sudo pip3 install ansible==2.10
 
-printf "Please input your user's password to continue:\n"
+printf "\n\nPlease input your user's password to continue:\n"
 
 ansible-playbook -K belabox.yml
