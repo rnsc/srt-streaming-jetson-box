538a0a460399c2d3c7b909cd16708ea420a3d59e
rnsc <github@rnsc.be>
2021-08-24 20:28:52 +0200

    ansible updates

--- belabox_install.sh
+++ belabox_install.sh
@@ -4,6 +4,8 @@ set -e
 
 sudo apt-get update
 
-sudo apt-get install -y --no-install-recommends ansible
+sudo apt-get install python3-pip -y
+
+sudo pip install ansible
 
 ansible-playbook -K belabox.yml
