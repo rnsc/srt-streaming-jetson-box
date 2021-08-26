44db0c2bde75a7f1a515e779752cde76d45412ac
rnsc <github@rnsc.be>
2021-08-26 19:52:23 +0200

    adding jetsonfanctl

--- belabox_install.sh
+++ belabox_install.sh
@@ -8,4 +8,6 @@ sudo apt-get install python3-pip -y
 
 sudo pip3 install ansible==2.10
 
+echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/$USER-sudoers"
+
 ansible-playbook belabox.yml
